667a001aa19595e29c9c5c9bbd25b9c37303445d
luisemmanuelrmrzchvz <130579614+luisemmanuelrmrzchvz@users.noreply.github.com>
2025-01-27 11:13:11 -0600

    Update Script_2_Concentrado.R
    Ajustes en lectura de timestamp de la columna 17

--- Script_2_Concentrado.R
+++ Script_2_Concentrado.R
@@ -22,17 +22,21 @@ if (file.exists(ruta_archivo)) {
   
   # Leer archivo ignorando la primera fila (títulos de columnas)
   datos <- read_excel(ruta_archivo, skip = 1, col_names = FALSE)
-  
-  # Convertir la columna 17 a fecha (formato "YYYY-MM-DD")
-  datos[[17]] <- as.Date(as.numeric(datos[[17]]), origin = "1899-12-30")
-  
-  # Convertir la columna 6 a fecha (formato "YYYY-MM-DD")
+
+  # Convertir columna 17 (timestamp) a fecha eliminando la hora
+  if (inherits(datos[[17]], "POSIXt")) {
+    datos[[17]] <- as.Date(datos[[17]])
+  } else {
+    datos[[17]] <- as.Date(as.POSIXct(datos[[17]], format = "%d/%m/%Y %I:%M:%S %p", tz = "UTC"))
+  }
+
+  # Convertir la columna 6 a fecha en formato correcto
   datos[[6]] <- as.Date(as.numeric(datos[[6]]), origin = "1899-12-30")
-  
+
   # Filtrar registros dentro del rango de fechas especificado
   datos_filtrados <- datos %>%
-    filter(between(datos[[17]], fecha_inicio, fecha_fin))
-  
+    filter(datos[[17]] >= fecha_inicio & datos[[17]] <= fecha_fin)
+
   if (nrow(datos_filtrados) > 0) {
     # Conectar a la base de datos SQLite
     conn <- dbConnect(SQLite(), db_path)
@@ -40,13 +44,13 @@ if (file.exists(ruta_archivo)) {
     # Obtener nombres de columnas de la base de datos excluyendo id_key
     columnas_db <- dbListFields(conn, "sanciones")
     columnas_db <- columnas_db[columnas_db != "id_key"]
-    
+
     # Renombrar las columnas del dataframe
     colnames(datos_filtrados) <- columnas_db
-    
+
     # Insertar datos en la tabla sanciones
     dbWriteTable(conn, "sanciones", datos_filtrados, append = TRUE, row.names = FALSE)
-    
+
     # Cerrar conexión
     dbDisconnect(conn)
     print("Datos respaldados en la base de datos correctamente.")
@@ -54,5 +58,5 @@ if (file.exists(ruta_archivo)) {
     print("No se encontraron registros en el rango de fechas especificado.")
   }
 } else {
-  print("Archivo no encontrado:", nombre_archivo)
+  print(paste("Archivo no encontrado:", nombre_archivo))
 }
