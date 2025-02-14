6c5ac21a3fc4d10c9aace07ff5afcded3555bde6
luisemmanuelrmrzchvz <lsmq.89@gmail.com>
2025-02-14 13:13:13 -0600

    nuevo error

--- 98 datos_pruebas.R
+++ 98 datos_pruebas.R
@@ -49,7 +49,7 @@ library(textstem)     # Para lematización
 library(widyr)        # Para n-gramas
 
 # 1. Cargar el archivo Excel y renombrar la columna
-ruta_archivo <- "C:/Users/racl26345/Documents/Tablas para Automatizaciones/Respuestas_abiertas.xlsx"
+ruta_archivo <- "C:/Users/racl26345/Documents/Tablas para Automatizaciones/Respuestas abiertas.xlsx"
 df <- read_excel(ruta_archivo, col_names = FALSE)  # Leer sin nombres de columna
 colnames(df) <- c("Respuesta_Abierta")  # Asignar el nombre "Respuesta_Abierta"
 
@@ -141,81 +141,119 @@ print(top_terms)
 ####################################################################################
 
 > library(readxl)       # Para leer archivos Excel
-Aviso:
-  package ‘readxl’ was built under R version 4.4.2 
 > library(tidyverse)    # Para manipulación de datos
-── Attaching core tidyverse packages ──────────────────────────────────────────────────────────────────────────────────────────────────────── tidyverse 2.0.0 ──
-✔ dplyr     1.1.4     ✔ readr     2.1.5
-✔ forcats   1.0.0     ✔ stringr   1.5.1
-✔ ggplot2   3.5.1     ✔ tibble    3.2.1
-✔ lubridate 1.9.4     ✔ tidyr     1.3.1
-✔ purrr     1.0.2     
-── Conflicts ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── tidyverse_conflicts() ──
-✖ dplyr::filter() masks stats::filter()
-✖ dplyr::lag()    masks stats::lag()
-ℹ Use the conflicted package to force all conflicts to become errors
-Avisos:
-  1: package ‘tidyverse’ was built under R version 4.4.2 
-2: package ‘ggplot2’ was built under R version 4.4.2 
-3: package ‘tidyr’ was built under R version 4.4.2 
-4: package ‘readr’ was built under R version 4.4.2 
-5: package ‘purrr’ was built under R version 4.4.2 
-6: package ‘dplyr’ was built under R version 4.4.2 
-7: package ‘forcats’ was built under R version 4.4.2 
-8: package ‘lubridate’ was built under R version 4.4.2 
 > library(tm)           # Para procesamiento de texto
-Cargando paquete requerido: NLP
-
-Adjuntando el paquete: ‘NLP’
-
-The following object is masked from ‘package:ggplot2’:
-  
-  annotate
-
-Avisos:
-  1: package ‘tm’ was built under R version 4.4.2 
-2: package ‘NLP’ was built under R version 4.4.2 
 > library(tidytext)     # Para tokenización y análisis de texto
-Aviso:
-  package ‘tidytext’ was built under R version 4.4.2 
 > library(syuzhet)      # Para análisis de sentimientos
-Aviso:
-  package ‘syuzhet’ was built under R version 4.4.2 
 > library(topicmodels)  # Para modelado de tópicos
-Aviso:
-  package ‘topicmodels’ was built under R version 4.4.2 
 > library(ggplot2)      # Para visualización
 > library(textstem)     # Para lematización
-Cargando paquete requerido: koRpus.lang.en
-Cargando paquete requerido: koRpus
-Cargando paquete requerido: sylly
-For information on available language packages for 'koRpus', run
-
-available.koRpus.lang()
-
-and see ?install.koRpus.lang()
-
-
-Adjuntando el paquete: ‘koRpus’
-
-The following object is masked from ‘package:tm’:
-  
-  readTagged
-
-The following object is masked from ‘package:readr’:
-  
-  tokenize
-
-Avisos:
-  1: package ‘textstem’ was built under R version 4.4.2 
-2: package ‘koRpus.lang.en’ was built under R version 4.4.2 
-3: package ‘koRpus’ was built under R version 4.4.2 
-4: package ‘sylly’ was built under R version 4.4.2 
 > library(widyr)        # Para n-gramas
-Aviso:
-  package ‘widyr’ was built under R version 4.4.2 
 > 
   > # 1. Cargar el archivo Excel y renombrar la columna
-  > ruta_archivo <- "C:/Users/racl26345/Documents/Tablas para Automatizaciones/Respuestas_abiertas.xlsx"
+  > ruta_archivo <- "C:/Users/racl26345/Documents/Tablas para Automatizaciones/Respuestas abiertas.xlsx"
 > df <- read_excel(ruta_archivo, col_names = FALSE)  # Leer sin nombres de columna
-Error: `path` does not exist: ‘C:/Users/racl26345/Documents/Tablas para Automatizaciones/Respuestas_abiertas.xlsx’
+New names:
+  • `` -> `...1`
+> colnames(df) <- c("Respuesta_Abierta")  # Asignar el nombre "Respuesta_Abierta"
+> 
+  > # Eliminar filas vacías
+  > df <- df %>% filter(!is.na(Respuesta_Abierta) & Respuesta_Abierta != "")
+> 
+  > # 2. Limpieza de datos
+  > df_limpio <- df %>%
+  +   mutate(Respuesta_Abierta = tolower(Respuesta_Abierta), # Convertir a minúsculas
+             +          Respuesta_Abierta = removePunctuation(Respuesta_Abierta), # Eliminar puntuación
+             +          Respuesta_Abierta = removeNumbers(Respuesta_Abierta), # Eliminar números
+             +          Respuesta_Abierta = stripWhitespace(Respuesta_Abierta)) %>% # Eliminar espacios extra
+  +   unnest_tokens(word, Respuesta_Abierta) %>%
+  +   filter(!word %in% stopwords("es")) # Eliminar stopwords en español
+> 
+  > # 3. Lematización
+  > df_limpio <- df_limpio %>%
+  +   mutate(word = lemmatize_strings(word, language = "es"))
+> 
+  > # 4. Análisis exploratorio: Palabras más comunes
+  > word_counts <- df_limpio %>%
+  +   count(word, sort = TRUE)
+> 
+  > # Ver las 10 palabras más comunes
+  > print(head(word_counts, 10))
+# A tibble: 10 × 2
+word            n
+<chr>       <int>
+  1 bien          279
+2 gracias       206
+3 informacion   198
+4 ninguna       189
+5 socio         173
+6 curso         152
+7 momento       141
+8 excelente     123
+9 comentarios   103
+10 ninguno       103
+> 
+  > # Visualización de palabras más comunes
+  > word_counts %>%
+  +   filter(n > 50) %>%
+  +   ggplot(aes(x = reorder(word, n), y = n)) +
+  +   geom_bar(stat = "identity", fill = "steelblue") +
+  +   coord_flip() +
+  +   labs(title = "Palabras más comunes en respuestas abiertas", x = "Palabra", y = "Frecuencia")
+> 
+  > # 5. Bigramas
+  > bigramas <- df %>%
+  +   unnest_tokens(bigrama, Respuesta_Abierta, token = "ngrams", n = 2) %>%
+  +   separate(bigrama, into = c("palabra1", "palabra2"), sep = " ") %>%
+  +   filter(!palabra1 %in% stopwords("es"),
+             +          !palabra2 %in% stopwords("es")) %>%
+  +   unite(bigrama, palabra1, palabra2, sep = " ") %>%
+  +   count(bigrama, sort = TRUE)
+> 
+  > # Visualización de bigramas
+  > bigramas %>%
+  +   filter(n > 20) %>%
+  +   ggplot(aes(x = reorder(bigrama, n), y = n)) +
+  +   geom_bar(stat = "identity", fill = "tomato") +
+  +   coord_flip() +
+  +   labs(title = "Bigramas más comunes en respuestas abiertas", x = "Bigrama", y = "Frecuencia")
+> 
+  > # 6. Análisis de sentimientos
+  > sentimientos <- get_nrc_sentiment(df$Respuesta_Abierta, language = "spanish")
+> 
+  > # Resumir sentimientos
+  > summary_sentimientos <- colSums(sentimientos)
+> print(summary_sentimientos)
+anger anticipation      disgust         fear          joy      sadness     surprise        trust     negative     positive 
+40          246           29          108          313          361           80          593          450         1243 
+> 
+  > # Visualización de sentimientos
+  > tibble(sentimiento = names(summary_sentimientos), total = summary_sentimientos) %>%
+  +   ggplot(aes(x = reorder(sentimiento, total), y = total, fill = sentimiento)) +
+  +   geom_bar(stat = "identity") +
+  +   coord_flip() +
+  +   labs(title = "Análisis de sentimientos", x = "Sentimiento", y = "Total")
+> 
+  > # 7. Modelado de tópicos (LDA)
+  > # Crear matriz de términos-documento (DTM)
+  > dtm <- df_limpio %>%
+  +   count(word, name = "freq") %>%
+  +   cast_dtm(document = 1, term = word, value = freq)
+Error in `distinct()`:
+  ! Must use existing variables.
+✖ `1` not found in `.data`.
+Run `rlang::last_trace()` to see where the error occurred.
+> rlang::last_trace()
+<error/rlang_error>
+  Error in `distinct()`:
+  ! Must use existing variables.
+✖ `1` not found in `.data`.
+---
+  Backtrace:
+  ▆
+1. ├─df_limpio %>% count(word, name = "freq") %>% ...
+2. └─tidytext::cast_dtm(., document = 1, term = word, value = freq)
+3.   └─tidytext::cast_sparse(data, !!document, !!term, !!value, ...)
+4.     ├─dplyr::distinct(data, !!sym(row_col), !!sym(column_col), .keep_all = TRUE)
+5.     └─dplyr:::distinct.data.frame(data, !!sym(row_col), !!sym(column_col), .keep_all = TRUE)
+Run rlang::last_trace(drop = FALSE) to see 2 hidden frames.
